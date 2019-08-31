614922b19bb22cb2902522c1336a64e677570b21
NorthwesternDirector <fanshundee@126.com>
2019-08-31 22:00:59 +0800

    readme修改

--- src/components/tabbar/MemberContainer.vue
+++ src/components/tabbar/MemberContainer.vue
@@ -1,13 +1,36 @@
 <template>
     <div>
         <h3>MemberContainer</h3>
+        <div id="subDiv">
+        <h4>哈哈</h4>
+        </div>
     </div>
+
+    
+
 </template>
 
 <script>
 
+
+var removeElement = function(nums, val) {
+    for(var i=0;i<nums.length;i++){
+        console.log(i);
+        if(nums[i]==val){
+            nums.splice(i,1);
+            i--;
+        }
+    };
+};
+removeElement([3,2,2,3],3)
+
 </script>
 
-<style lang="sass" scoped>
-
+<style lang="scss" scoped>
+$color:rgb(253, 184, 255);
+$className:subDiv; 
+div{
+    ##{$className}{color:$color;}
+}
 </style>
+
